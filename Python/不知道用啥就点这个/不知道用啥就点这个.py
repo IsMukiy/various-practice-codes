6df4e08c81158aff5b442581ca5515dc22fb1e4c
mukiy <wsxqqy@163.com>
2025-08-26 08:26:44 +0800

    更新了"/Python/不知道用啥就点这个"里的"不知道用啥就点这个.py"
    在代码里添加了MIT协议~

--- Python/不知道用啥就点这个/不知道用啥就点这个.py
+++ Python/不知道用啥就点这个/不知道用啥就点这个.py
@@ -3,6 +3,33 @@
 
 
 
+'''
+MIT License
+
+Copyright (c) 2025 IsMukiy
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+
+
+
 r'''
 README
 语言/Language:
@@ -339,4 +366,5 @@ try:
         print('-------新会话----------')#提示新会话开始
 
 except Exception as e:#如果发生错误
+
     error(e)
